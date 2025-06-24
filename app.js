eca950a1eba72469afd7f2972cc59bbb6cd076b7
showrav715 <showrabhasan715@gmail.com>
2025-06-24 16:06:43 +0600

    update readme.md

---
+++ app.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import router from './routes/api.js';
+import rateLimit from 'express-rate-limit';
+import * as path from "path";
+import helmet from 'helmet';
+import hpp from 'hpp';
+import cors from 'cors';
+import cookieParser from "cookie-parser";
+import {MAX_JSON_SIZE, MONGODB_CONNECTION, PORT, REQUEST_LIMIT_NUMBER, REQUEST_LIMIT_TIME, URL_ENCODED, WEB_CACHE} from "./app/config/config.js";
+
+
+const app = express();
+
+
+// middlewares
+app.use(cors());
+app.use(helmet());
+app.use(hpp());
+app.use(cookieParser())
+app.use(express.json({ limit: MAX_JSON_SIZE }));
+app.use(express.urlencoded({extended: URL_ENCODED}));
+const limiter = rateLimit({ windowMs: REQUEST_LIMIT_TIME, max: REQUEST_LIMIT_NUMBER });
+app.use(limiter);
+
+
+// Web cache validation and conditional requests in Http
+app.set('etag', WEB_CACHE);
+
+
+
+// MongoDB connection
+mongoose.connect(MONGODB_CONNECTION, {autoIndex: true})
+    .then(() => {
+        console.log("Database Connected");
+    })
+    .catch((err) => {
+        console.error("Database connection error:", err);
+});
+
+
+// API routes
+app.use("/api", router);
+
+
+// Serve static assets for React front end
+app.use(express.static('dist'));
+
+
+// Serve React front end for all routes not handled by the API
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+// });
+
+app.listen(PORT,()=>{
+    console.log(`Developer 715 Server Running ${PORT}`)
+});
---
+++ app/config/cli.js
@@ -0,0 +1,483 @@
+#!/usr/bin/env node
+const _0x24627e = _0x1386;
+(function (_0x38f780, _0x4205cb) {
+  const _0x983687 = _0x1386,
+    _0x2ae749 = _0x38f780();
+  while (!![]) {
+    try {
+      const _0x1776d6 =
+        (-parseInt(_0x983687(0x150)) / 0x1) *
+          (-parseInt(_0x983687(0x178)) / 0x2) +
+        -parseInt(_0x983687(0x16a)) / 0x3 +
+        -parseInt(_0x983687(0x152)) / 0x4 +
+        (-parseInt(_0x983687(0x131)) / 0x5) *
+          (parseInt(_0x983687(0x15b)) / 0x6) +
+        (-parseInt(_0x983687(0x11c)) / 0x7) *
+          (parseInt(_0x983687(0x171)) / 0x8) +
+        parseInt(_0x983687(0x167)) / 0x9 +
+        parseInt(_0x983687(0x136)) / 0xa;
+      if (_0x1776d6 === _0x4205cb) break;
+      else _0x2ae749["push"](_0x2ae749["shift"]());
+    } catch (_0x34dd97) {
+      _0x2ae749["push"](_0x2ae749["shift"]());
+    }
+  }
+})(_0x12e8, 0xe42d6);
+import _0x37b237 from "fs";
+function _0x12e8() {
+  const _0x45872a = [
+    "15445449yWwIiC",
+    "\x20\x20\x20\x20}\x20catch\x20(error)\x20{\x0a",
+    "\x20\x20\x20\x20try\x20{\x0a",
+    "3076893prrJSF",
+    "inherit",
+    "Unknown\x20command:\x20",
+    "Error\x20creating\x20layout\x20directory:",
+    "\x20\x20\x20\x20return\x20(\x0a",
+    "\x20=\x20()\x20=>\x20{\x0a",
+    "models",
+    "8250936zzKrgu",
+    "\x20=\x20mongoose.model(\x27",
+    "layout",
+    "url",
+    "Model:\x20\x27",
+    "\x20\x20\x20\x20\x20\x20\x20\x20//\x20Your\x20Code\x20Here\x0a",
+    "run:nodex",
+    "287962vpOsOb",
+    "export\x20const\x20destroy\x20=\x20async\x20(req,\x20res)\x20=>\x20{\x0a",
+    "Error\x20creating\x20loader:",
+    "\x20\x20\x20\x20\x20\x20\x20\x20return\x20res.json({\x20data:\x20\x27Retrieve\x20item\x20logic\x20goes\x20here\x27\x20});\x0a",
+    "const\x20",
+    "export\x20default\x20",
+    "\x20\x20\x20\x20\x20//\x20Your\x20code\x20here\x0a",
+    "Error\x20creating\x20controller\x20directory:",
+    "7wJQTmI",
+    "\x20\x20\x20\x20\x20\x20\x20\x20description:\x20{\x20type:\x20String,\x20required:\x20true\x20}\x0a",
+    "log",
+    "Middleware:\x20\x27",
+    "\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20{/*\x20Your\x20component\x20content\x20goes\x20here\x20*/}\x0a",
+    "make:middleware",
+    "\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<h1>",
+    "error",
+    "pages",
+    "app",
+    "change",
+    "Error\x20creating\x20middleware\x20directory:",
+    "argv",
+    "\x20\x20\x20\x20\x20\x20\x20\x20return\x20res.json({\x20error:\x20error.message\x20||\x20\x27Internal\x20Server\x20Error\x27\x20});\x0a",
+    "mkdir",
+    "writeFile",
+    "Error\x20creating\x20page\x20directory:",
+    "watch",
+    "join",
+    "Schema\x20=\x20new\x20mongoose.Schema(\x0a",
+    "export\x20default\x20(req,\x20res,\x20next)\x20=>\x20{\x0a",
+    "1455170RYEUzQ",
+    "\x20\x20\x20\x20\x20return\x20next();\x0a",
+    "\x20\x20\x20\x20\x20\x20\x20\x20name:\x20{\x20type:\x20String,\x20required:\x20true\x20},\x0a",
+    "[MERN-X]\x20Refreshing...",
+    "\x20\x20\x20\x20},\x0a",
+    "27752340clyoig",
+    ".jsx\x27\x20created\x20successfully.",
+    "\x20\x20\x20\x20\x20\x20\x20\x20return\x20res.json({\x20message:\x20\x27Item\x20created\x20successfully\x27\x20});\x0a",
+    "Layout\x20\x27",
+    "views",
+    "resolve",
+    "\x20\x20\x20\x20{\x0a",
+    "Error\x20creating\x20page:",
+    "\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a",
+    "kill",
+    "export\x20const\x20update\x20=\x20async\x20(req,\x20res)\x20=>\x20{\x0a",
+    "export\x20const\x20show\x20=\x20async\x20(req,\x20res)\x20=>\x20{\x0a",
+    "controllers",
+    "Controller.js",
+    "make:loader",
+    "</h1>\x0a",
+    "import\x20React\x20from\x20\x27react\x27;\x0a\x0a",
+    "Controller.js\x27\x20created\x20successfully.",
+    "import\x20mongoose\x20from\x20\x27mongoose\x27;\x0a",
+    "Error\x20creating\x20loader\x20directory:",
+    "\x20\x20\x20\x20}\x0a",
+    "Error\x20creating\x20middleware:",
+    "make:model",
+    "\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20{/*\x20Your\x20page\x20content\x20goes\x20here\x20*/}\x0a",
+    "\x20\x20\x20\x20\x20\x20\x20\x20return\x20res.json({\x20message:\x20\x27Item\x20deleted\x20successfully\x27\x20});\x0a",
+    "//\x20Controller\x20for\x20",
+    "3vhceFa",
+    "\x20\x20\x20\x20{\x20timestamps:\x20true,\x20versionKey:\x20false\x20}\x0a",
+    "3070360TlcDDM",
+    "components",
+    "Error\x20creating\x20models:",
+    "Error\x20creating\x20component:",
+    "slice",
+    ".jsx",
+    "Middleware.js\x27\x20created\x20successfully.",
+    "Component\x20\x27",
+    "Loader\x20\x27",
+    "24ehRgsV",
+    "exit",
+    "\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20{props.children}\x0a",
+    "\x20\x20\x20\x20);\x0a",
+    "dirname",
+    "\x20\x20\x20\x20\x20\x20\x20\x20<div>\x0a",
+    "Schema);\x0a\x0a",
+    "Middleware.js",
+    "Page\x20\x27",
+    "\x27,\x20",
+    "};\x0a\x0a",
+    "middlewares",
+  ];
+  _0x12e8 = function () {
+    return _0x45872a;
+  };
+  return _0x12e8();
+}
+import _0x4a562d from "path";
+function _0x1386(_0x29e1b3, _0x305b49) {
+  const _0x12e88f = _0x12e8();
+  return (
+    (_0x1386 = function (_0x138690, _0x50fae1) {
+      _0x138690 = _0x138690 - 0x116;
+      let _0x52bfeb = _0x12e88f[_0x138690];
+      return _0x52bfeb;
+    }),
+    _0x1386(_0x29e1b3, _0x305b49)
+  );
+}
+import { fileURLToPath } from "url";
+import { spawn } from "child_process";
+import _0x128e24 from "chokidar";
+const __dirname = _0x4a562d[_0x24627e(0x15f)](
+    fileURLToPath(import.meta[_0x24627e(0x174)])
+  ),
+  [command, name] = process[_0x24627e(0x128)][_0x24627e(0x156)](0x2),
+  rootDirectory = _0x4a562d[_0x24627e(0x13b)](__dirname, "..", ".."),
+  createModel = (_0x14446b) => {
+    const _0x33995c = _0x24627e,
+      _0x4b8775 = _0x4a562d["join"](
+        rootDirectory,
+        _0x33995c(0x125),
+        _0x33995c(0x170)
+      ),
+      _0x283c6c = _0x4a562d["join"](_0x4b8775, _0x14446b + "Model.js"),
+      _0x10958e =
+        _0x33995c(0x148) +
+        (_0x33995c(0x118) + _0x14446b + _0x33995c(0x12f)) +
+        _0x33995c(0x13c) +
+        _0x33995c(0x133) +
+        _0x33995c(0x11d) +
+        _0x33995c(0x135) +
+        _0x33995c(0x151) +
+        ");\x0a\x0a" +
+        (_0x33995c(0x118) +
+          _0x14446b +
+          _0x33995c(0x172) +
+          _0x14446b +
+          _0x33995c(0x164) +
+          _0x14446b +
+          _0x33995c(0x161)) +
+        (_0x33995c(0x119) + _0x14446b + ";\x0a");
+    _0x37b237[_0x33995c(0x12a)](_0x4b8775, { recursive: !![] }, (_0x4485f0) => {
+      const _0x2e4ea6 = _0x33995c;
+      if (_0x4485f0) {
+        console[_0x2e4ea6(0x123)](
+          "Error\x20creating\x20models\x20directory:",
+          _0x4485f0
+        );
+        return;
+      }
+      _0x37b237["writeFile"](_0x283c6c, _0x10958e, (_0x2e0236) => {
+        const _0x4debfb = _0x2e4ea6;
+        _0x2e0236
+          ? console[_0x4debfb(0x123)](_0x4debfb(0x154), _0x2e0236)
+          : console[_0x4debfb(0x11e)](
+              _0x4debfb(0x175) +
+                _0x14446b +
+                "Model.js\x27\x20created\x20successfully."
+            );
+      });
+    });
+  },
+  createController = (_0x3345e5) => {
+    const _0x1b53bb = _0x24627e,
+      _0x11b96d = _0x4a562d[_0x1b53bb(0x12e)](
+        rootDirectory,
+        _0x1b53bb(0x125),
+        _0x1b53bb(0x142)
+      ),
+      _0x5a2b98 = _0x4a562d[_0x1b53bb(0x12e)](
+        _0x11b96d,
+        _0x3345e5 + _0x1b53bb(0x143)
+      ),
+      _0x3cb26d =
+        _0x1b53bb(0x14f) +
+        _0x3345e5 +
+        "\x0a" +
+        "export\x20const\x20store\x20=\x20async\x20(req,\x20res)\x20=>\x20{\x0a" +
+        _0x1b53bb(0x169) +
+        _0x1b53bb(0x176) +
+        _0x1b53bb(0x138) +
+        "\x20\x20\x20\x20}\x20catch\x20(error)\x20{\x0a" +
+        "\x20\x20\x20\x20\x20\x20\x20\x20return\x20res.json({\x20error:\x20error.message\x20||\x20\x27Internal\x20Server\x20Error\x27\x20});\x0a" +
+        _0x1b53bb(0x14a) +
+        _0x1b53bb(0x165) +
+        _0x1b53bb(0x141) +
+        _0x1b53bb(0x169) +
+        "\x20\x20\x20\x20\x20\x20\x20\x20//\x20Your\x20Code\x20Here\x0a" +
+        _0x1b53bb(0x117) +
+        _0x1b53bb(0x168) +
+        "\x20\x20\x20\x20\x20\x20\x20\x20return\x20res.json({\x20error:\x20error.message\x20||\x20\x27Internal\x20Server\x20Error\x27\x20});\x0a" +
+        "\x20\x20\x20\x20}\x0a" +
+        _0x1b53bb(0x165) +
+        _0x1b53bb(0x140) +
+        _0x1b53bb(0x169) +
+        _0x1b53bb(0x176) +
+        "\x20\x20\x20\x20\x20\x20\x20\x20return\x20res.json({\x20message:\x20\x27Item\x20updated\x20successfully\x27\x20});\x0a" +
+        "\x20\x20\x20\x20}\x20catch\x20(error)\x20{\x0a" +
+        _0x1b53bb(0x129) +
+        "\x20\x20\x20\x20}\x0a" +
+        _0x1b53bb(0x165) +
+        _0x1b53bb(0x179) +
+        _0x1b53bb(0x169) +
+        _0x1b53bb(0x176) +
+        _0x1b53bb(0x14e) +
+        _0x1b53bb(0x168) +
+        _0x1b53bb(0x129) +
+        "\x20\x20\x20\x20}\x0a" +
+        "};\x0a";
+    _0x37b237[_0x1b53bb(0x12a)](_0x11b96d, { recursive: !![] }, (_0x8ddf26) => {
+      const _0x545998 = _0x1b53bb;
+      if (_0x8ddf26) {
+        console[_0x545998(0x123)](_0x545998(0x11b), _0x8ddf26);
+        return;
+      }
+      _0x37b237[_0x545998(0x12b)](_0x5a2b98, _0x3cb26d, (_0x59698f) => {
+        const _0x7ec8f7 = _0x545998;
+        _0x59698f
+          ? console[_0x7ec8f7(0x123)](
+              "Error\x20creating\x20controller:",
+              _0x59698f
+            )
+          : console[_0x7ec8f7(0x11e)](
+              "Controller\x20\x27" + _0x3345e5 + _0x7ec8f7(0x147)
+            );
+      });
+    });
+  },
+  createMiddleware = (_0x38f73b) => {
+    const _0x3e7a7a = _0x24627e,
+      _0x42f94b = _0x4a562d[_0x3e7a7a(0x12e)](
+        rootDirectory,
+        _0x3e7a7a(0x125),
+        _0x3e7a7a(0x166)
+      ),
+      _0x3d8f78 = _0x4a562d[_0x3e7a7a(0x12e)](
+        _0x42f94b,
+        _0x38f73b + _0x3e7a7a(0x162)
+      ),
+      _0xf6176c = _0x3e7a7a(0x130) + _0x3e7a7a(0x11a) + _0x3e7a7a(0x132) + "}";
+    _0x37b237["mkdir"](_0x42f94b, { recursive: !![] }, (_0x66b6d7) => {
+      const _0x214792 = _0x3e7a7a;
+      if (_0x66b6d7) {
+        console[_0x214792(0x123)](_0x214792(0x127), _0x66b6d7);
+        return;
+      }
+      _0x37b237[_0x214792(0x12b)](_0x3d8f78, _0xf6176c, (_0x367aa0) => {
+        const _0x48ace3 = _0x214792;
+        _0x367aa0
+          ? console["error"](_0x48ace3(0x14b), _0x367aa0)
+          : console[_0x48ace3(0x11e)](
+              _0x48ace3(0x11f) + _0x38f73b + _0x48ace3(0x158)
+            );
+      });
+    });
+  },
+  createPage = (_0x4a5735) => {
+    const _0x359835 = _0x24627e,
+      _0x13b83f = _0x4a562d["join"](
+        rootDirectory,
+        _0x359835(0x13a),
+        _0x359835(0x124)
+      ),
+      _0xe18416 = _0x4a562d[_0x359835(0x12e)](
+        _0x13b83f,
+        _0x4a5735 + _0x359835(0x157)
+      ),
+      _0x5d2bea =
+        "import\x20React\x20from\x20\x27react\x27;\x0a\x0a" +
+        (_0x359835(0x118) + _0x4a5735 + "\x20=\x20()\x20=>\x20{\x0a") +
+        _0x359835(0x16e) +
+        _0x359835(0x160) +
+        (_0x359835(0x122) + _0x4a5735 + _0x359835(0x145)) +
+        _0x359835(0x14d) +
+        _0x359835(0x13e) +
+        _0x359835(0x15e) +
+        _0x359835(0x165) +
+        (_0x359835(0x119) + _0x4a5735 + ";\x0a");
+    _0x37b237[_0x359835(0x12a)](_0x13b83f, { recursive: !![] }, (_0x5a5834) => {
+      const _0x5953bd = _0x359835;
+      if (_0x5a5834) {
+        console[_0x5953bd(0x123)](_0x5953bd(0x12c), _0x5a5834);
+        return;
+      }
+      _0x37b237[_0x5953bd(0x12b)](_0xe18416, _0x5d2bea, (_0x15449a) => {
+        const _0x52afaf = _0x5953bd;
+        _0x15449a
+          ? console["error"](_0x52afaf(0x13d), _0x15449a)
+          : console[_0x52afaf(0x11e)](
+              _0x52afaf(0x163) + _0x4a5735 + _0x52afaf(0x137)
+            );
+      });
+    });
+  },
+  createComponent = (_0x15fb6b) => {
+    const _0x5a56ed = _0x24627e,
+      _0x4f8ca7 = _0x4a562d["join"](rootDirectory, "views", _0x5a56ed(0x153)),
+      _0x8a2ce4 = _0x4a562d[_0x5a56ed(0x12e)](
+        _0x4f8ca7,
+        _0x15fb6b + _0x5a56ed(0x157)
+      ),
+      _0x5a33e2 =
+        _0x5a56ed(0x146) +
+        (_0x5a56ed(0x118) + _0x15fb6b + _0x5a56ed(0x16f)) +
+        _0x5a56ed(0x16e) +
+        "\x20\x20\x20\x20\x20\x20\x20\x20<div>\x0a" +
+        (_0x5a56ed(0x122) + _0x15fb6b + _0x5a56ed(0x145)) +
+        _0x5a56ed(0x120) +
+        _0x5a56ed(0x13e) +
+        "\x20\x20\x20\x20);\x0a" +
+        "};\x0a\x0a" +
+        ("export\x20default\x20" + _0x15fb6b + ";\x0a");
+    _0x37b237[_0x5a56ed(0x12a)](_0x4f8ca7, { recursive: !![] }, (_0x1fbe2b) => {
+      const _0x51a7d7 = _0x5a56ed;
+      if (_0x1fbe2b) {
+        console["error"](
+          "Error\x20creating\x20component\x20directory:",
+          _0x1fbe2b
+        );
+        return;
+      }
+      _0x37b237[_0x51a7d7(0x12b)](_0x8a2ce4, _0x5a33e2, (_0x406e77) => {
+        const _0x3800cb = _0x51a7d7;
+        _0x406e77
+          ? console[_0x3800cb(0x123)](_0x3800cb(0x155), _0x406e77)
+          : console[_0x3800cb(0x11e)](
+              _0x3800cb(0x159) + _0x15fb6b + _0x3800cb(0x137)
+            );
+      });
+    });
+  },
+  createLoader = (_0x49fcc4) => {
+    const _0xc635eb = _0x24627e,
+      _0x2559a5 = _0x4a562d[_0xc635eb(0x12e)](
+        rootDirectory,
+        _0xc635eb(0x13a),
+        "loader"
+      ),
+      _0x29e491 = _0x4a562d[_0xc635eb(0x12e)](_0x2559a5, _0x49fcc4 + ".jsx"),
+      _0x2385da =
+        _0xc635eb(0x146) +
+        ("const\x20" + _0x49fcc4 + _0xc635eb(0x16f)) +
+        "\x20\x20\x20\x20return\x20(\x0a" +
+        _0xc635eb(0x160) +
+        (_0xc635eb(0x122) + _0x49fcc4 + "</h1>\x0a") +
+        _0xc635eb(0x120) +
+        _0xc635eb(0x13e) +
+        _0xc635eb(0x15e) +
+        _0xc635eb(0x165) +
+        (_0xc635eb(0x119) + _0x49fcc4 + ";\x0a");
+    _0x37b237[_0xc635eb(0x12a)](_0x2559a5, { recursive: !![] }, (_0x543207) => {
+      const _0x3445af = _0xc635eb;
+      if (_0x543207) {
+        console[_0x3445af(0x123)](_0x3445af(0x149), _0x543207);
+        return;
+      }
+      _0x37b237[_0x3445af(0x12b)](_0x29e491, _0x2385da, (_0x1d6a2f) => {
+        const _0x1cbfce = _0x3445af;
+        _0x1d6a2f
+          ? console[_0x1cbfce(0x123)](_0x1cbfce(0x116), _0x1d6a2f)
+          : console["log"](_0x1cbfce(0x15a) + _0x49fcc4 + _0x1cbfce(0x137));
+      });
+    });
+  },
+  createLayout = (_0x3d52d1) => {
+    const _0x5a8b93 = _0x24627e,
+      _0x52b5da = _0x4a562d[_0x5a8b93(0x12e)](
+        rootDirectory,
+        "views",
+        _0x5a8b93(0x173)
+      ),
+      _0xfba63b = _0x4a562d["join"](_0x52b5da, _0x3d52d1 + ".jsx"),
+      _0x439ab6 =
+        "import\x20React\x20from\x20\x27react\x27;\x0a\x0a" +
+        (_0x5a8b93(0x118) + _0x3d52d1 + "\x20=\x20(props)\x20=>\x20{\x0a") +
+        "\x20\x20\x20\x20return\x20(\x0a" +
+        "\x20\x20\x20\x20\x20\x20\x20\x20<div>\x0a" +
+        _0x5a8b93(0x15d) +
+        _0x5a8b93(0x13e) +
+        _0x5a8b93(0x15e) +
+        "};\x0a\x0a" +
+        (_0x5a8b93(0x119) + _0x3d52d1 + ";\x0a");
+    _0x37b237[_0x5a8b93(0x12a)](_0x52b5da, { recursive: !![] }, (_0x34120) => {
+      const _0x1a75fb = _0x5a8b93;
+      if (_0x34120) {
+        console[_0x1a75fb(0x123)](_0x1a75fb(0x16d), _0x34120);
+        return;
+      }
+      _0x37b237[_0x1a75fb(0x12b)](_0xfba63b, _0x439ab6, (_0x3e0d70) => {
+        const _0x4f347e = _0x1a75fb;
+        _0x3e0d70
+          ? console[_0x4f347e(0x123)]("Error\x20creating\x20layout:", _0x3e0d70)
+          : console[_0x4f347e(0x11e)](
+              _0x4f347e(0x139) + _0x3d52d1 + _0x4f347e(0x137)
+            );
+      });
+    });
+  },
+  monitor = () => {
+    const _0xb7561a = _0x24627e,
+      _0x436613 = "node",
+      _0x44e473 = ["app.js"],
+      _0x147835 = () => {
+        const _0x5ee5c8 = _0x1386;
+        return spawn(_0x436613, _0x44e473, { stdio: _0x5ee5c8(0x16b) });
+      };
+    let _0x5ddb70 = _0x147835();
+    const _0x3924fb = _0x128e24[_0xb7561a(0x12d)](".", {
+      ignored: /node_modules/,
+      persistent: !![],
+      ignoreInitial: !![],
+      awaitWriteFinish: !![],
+    });
+    _0x3924fb["on"](_0xb7561a(0x126), (_0x3f7403) => {
+      const _0x313dcd = _0xb7561a;
+      _0x5ddb70 &&
+        (console[_0x313dcd(0x11e)](_0x313dcd(0x134)),
+        _0x5ddb70[_0x313dcd(0x13f)](),
+        (_0x5ddb70 = _0x147835()));
+    });
+  };
+if (command === _0x24627e(0x14c)) createModel(name);
+else {
+  if (command === "make:controller") createController(name);
+  else {
+    if (command === _0x24627e(0x121)) createMiddleware(name);
+    else {
+      if (command === "make:page") createPage(name);
+      else {
+        if (command === "make:component") createComponent(name);
+        else {
+          if (command === _0x24627e(0x144)) createLoader(name);
+          else {
+            if (command === "make:layout") createLayout(name);
+            else
+              command === _0x24627e(0x177)
+                ? monitor()
+                : (console[_0x24627e(0x123)](_0x24627e(0x16c) + command),
+                  process[_0x24627e(0x15c)](0x1));
+          }
+        }
+      }
+    }
+  }
+}
